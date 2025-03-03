a751997ff8cd021b1c0fb5b3d133a705c38ca25b
Fabien Roy <120800630+fabry44@users.noreply.github.com>
2025-03-03 16:13:15 +0100

    feat: Add directory checks and copy generated files to dist for Netlify deployment

--- generate-resume.js
+++ generate-resume.js
@@ -6,27 +6,30 @@ import * as fs from 'fs';
 
 // Obtenir le chemin du répertoire courant
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const resumeJsonPath = path.join(__dirname, 'resume.json');
-const outputDir = path.join(__dirname, 'public');
-const themePath = path.join(__dirname, 'themes', 'jsonresume-theme-macchiato/index.js');
+const outputDir = path.join(__dirname, 'public');  
+const distDir = path.join(__dirname, 'dist'); // Dossier final Netlify
+
+// Vérifier que les dossiers existent
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
+}
+if (!fs.existsSync(distDir)) {
+    fs.mkdirSync(distDir, { recursive: true });
+}
 
 // Commande pour générer le CV en HTML
-const generateHtmlCommand = `npx resumed render --theme ${themePath} --output ${path.join(outputDir, 'cv_fabien_Roy.html')}`;
+const generateHtmlCommand = `npx resumed render --theme jsonresume-theme-macchiato --output ${path.join(outputDir, 'cv_fabien_roy.html')}`;
 
-// Exécuter la commande pour générer le CV en HTML
 exec(generateHtmlCommand, async (error, stdout, stderr) => {
   if (error) {
     console.error(`Error generating HTML: ${error.message}`);
     return;
   }
-  if (stderr) {
-    console.error(`HTML generation stderr: ${stderr}`);
-  }
   console.log(`HTML generated successfully: ${stdout}`);
 
   // Lire le fichier HTML généré
-  const htmlPath = path.join(outputDir, 'cv_fabien_Roy.html');
-  let htmlContent = fs.readFileSync(htmlPath, 'utf8');
+  const htmlPath = path.join(outputDir, 'cv_fabien_roy.html');
+  const htmlContent = fs.readFileSync(htmlPath, 'utf8');
 
   // Lancer Puppeteer pour générer le PDF
   const browser = await puppeteer.launch({
@@ -34,16 +37,21 @@ exec(generateHtmlCommand, async (error, stdout, stderr) => {
   });
   const page = await browser.newPage();
 
-  // Charger la page HTML modifiée
+  // Charger la page HTML
   await page.setContent(htmlContent, { waitUntil: 'networkidle2' });
 
   // Générer le PDF
   await page.pdf({
-    path: path.join(outputDir, 'cv_fabien_Roy.pdf'),
+    path: path.join(outputDir, 'cv_fabien_roy.pdf'),
     format: 'A4',
     margin: { top: '0', right: '0', bottom: '0', left: '0' },
   });
 
   await browser.close();
-  console.log('PDF generated successfully with custom CSS');
+  console.log('PDF generated successfully');
+
+  // Copier les fichiers dans `dist/` pour qu'ils soient pris en compte par Netlify
+  fs.copyFileSync(path.join(outputDir, 'cv_fabien_roy.html'), path.join(distDir, 'cv_fabien_roy.html'));
+  fs.copyFileSync(path.join(outputDir, 'cv_fabien_roy.pdf'), path.join(distDir, 'cv_fabien_roy.pdf'));
+  console.log('Files copied to dist/');
 });
