c64055dfc20e35a2f18b84f9148b9712d05f860a
Fabien Roy <120800630+fabry44@users.noreply.github.com>
2025-03-17 01:05:33 +0100

    Ajout de la configuration Netlify et mise à jour de la configuration Astro pour un rendu hybride

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'hybrid', // Permet de garder les pages statiques mais avec des routes dynamiques si besoin
