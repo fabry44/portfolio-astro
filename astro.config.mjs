52eeb1a3440da1e13ae196a537fa11f833500f3f
Fabien Roy <120800630+fabry44@users.noreply.github.com>
2025-03-17 20:24:43 +0100

    Add Matomo integration for analytics tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,28 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
+import matomo from 'astro-matomo';
 
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
   site: 'https://fabien-roy.fr',
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    matomo({
+      enabled: import.meta.env.PROD, // Only load in production
+      host: "https://analytics.fabien-roy.fr/",
+      setCookieDomain: "*.fabien-roy.fr",
+      trackerUrl: "js/", // defaults to matomo.php
+      srcUrl: "js/", // defaults to matomo.js
+      siteId: 666,
+      heartBeatTimer: 5,
+      disableCookies: true,
+      debug: false,
+      viewTransition: {
+        contentElement: "main"
+      }
+    }),
+  ],
 });
