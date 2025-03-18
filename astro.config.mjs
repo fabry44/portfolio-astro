30acf3f76d51f2ff9f021faba7cf854361d70270
Fabien Roy <120800630+fabry44@users.noreply.github.com>
2025-03-18 15:36:55 +0100

    Update Astro configuration for Netlify compatibility and adjust output settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import netlify from '@astrojs/netlify';
 import matomo from 'astro-matomo';
 
 export default defineConfig({
-  output: 'static', // CHANGER en "static" pour éviter les erreurs Netlify
-  
+  output: 'server', // CHANGER en "static" pour éviter les erreurs Netlify
+  adapter: netlify({
+    edge: false, // Empêche Netlify de forcer certaines optimisations côté serveur
+  }),
   site: 'https://fabien-roy.fr',
   integrations: [
     tailwind(),
