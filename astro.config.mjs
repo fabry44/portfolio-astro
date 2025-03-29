e21beebd989979291b4140f4a8a60abf074d41de
Fabien Roy <120800630+fabry44@users.noreply.github.com>
2025-03-29 15:01:25 +0100

    Mise à jour des fichiers d'images et ajout de la prise en charge du sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,30 +4,28 @@ import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
 import matomo from 'astro-matomo';
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   output: 'server', // CHANGER en "static" pour éviter les erreurs Netlify
   adapter: netlify({
     edge: false, // Empêche Netlify de forcer certaines optimisations côté serveur
   }),
   site: 'https://fabien-roy.fr',
-  integrations: [
-    tailwind(),
-    react(),
-    matomo({
-      enabled: import.meta.env.PROD, 
-      host: "https://analytics.fabien-roy.fr/",
-      setCookieDomain: "*.fabien-roy.fr",
-      trackerUrl: "matomo.php",
-      srcUrl: "matomo.js",
-      siteId: 1,
-      heartBeatTimer: 5,
-      disableCookies: true,
-      debug: false,
-      viewTransition: {
-        contentElement: "main"
-      }
-    }),
-  ],
+  integrations: [tailwind(), react(), matomo({
+    enabled: import.meta.env.PROD, 
+    host: "https://analytics.fabien-roy.fr/",
+    setCookieDomain: "*.fabien-roy.fr",
+    trackerUrl: "matomo.php",
+    srcUrl: "matomo.js",
+    siteId: 1,
+    heartBeatTimer: 5,
+    disableCookies: true,
+    debug: false,
+    viewTransition: {
+      contentElement: "main"
+    }
+  }), sitemap()],
   build: {
     minify: true, // Désactive temporairement la minification pour tester
   },
