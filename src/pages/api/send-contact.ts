108bb3b7a9c713b135818580e91ba852da999fa6
Fabien Roy <120800630+fabry44@users.noreply.github.com>
2025-05-14 15:08:36 +0200

    Correction de l'URL de récupération du token OAuth2 dans la fonction POST

--- src/pages/api/send-contact.ts
+++ src/pages/api/send-contact.ts
@@ -10,7 +10,7 @@ export async function POST({ request }: APIContext) {
         const formData = await request.json();
 
         // 🔐 Récupérer un token sécurisé
-        const oauthResponse = await fetch("https://dashboard.fabien-roy.fr/token", {
+        const oauthResponse = await fetch("https://backoffice.fabien-roy.fr/token", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
